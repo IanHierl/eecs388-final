e98e1d9b4780bef77d2dfc7129d87e6e51f5a003
Ian Hierl <IanHierl@users.noreply.github.com>
2019-05-15 18:20:48 -0500

    Final scan_script.py

--- scan_script.py
+++ scan_script.py
@@ -1,22 +1,29 @@
+from mpl_toolkits.mplot3d import Axes3D
 import serial
 import matplotlib.pyplot as plt
+
+def getLineAsStr():
+	return str( ser.readline(), "utf-8" ).replace( "\r\n", "" )
 
 def handleScan():
 	xs = []
 	ys = []
 	zs = []
-	i = 0
 	ser.write( b'h' )
-	print( ser.readline() )
-	curline = ser.readline()
-	while( curline is not "END" ):
+	print( getLineAsStr() )
+	print( getLineAsStr() )
+	curline = getLineAsStr()
+	while( True ):
+		print( curline )
+		if( "END" in curline ):
+			break
 		[ x, y, z ] = curline.split("\t")
-		xs[ i ] = x
-		ys[ i ] = y
-		zs[ i ] = z
-		i = i + 1
-		curline = ser.readline()
-	return ( xs, ys, zs )
+		[ x, y, z ] = [ int(x), int(y), int(z) ]
+		xs.append( x )
+		ys.append( y )
+		zs.append( z )
+		curline = getLineAsStr()
+	return [ xs, ys, zs ]
 
 
 if __name__ == "__main__":
@@ -29,10 +36,12 @@ if __name__ == "__main__":
 			print( "Cannot open serial port... try again" )
 	input( "Press enter to begin scan..." )
 	fig = plt.figure()
-	ax = fig.add_subplot( 111, projection = '3d' )
-	( xs, ys, zs ) = handleScan()
+	ax = Axes3D( fig )
+	[ xs, ys, zs ] = handleScan()
+	ser.close()
 	ax.scatter( xs, ys, zs )
 	ax.set_xlabel( "X Axis" )
 	ax.set_ylabel( "Y Axis" )
 	ax.set_zlabel( "Z Axis" )
+	input( "Data plotted. Press enter to display..." )
 	plt.show()
